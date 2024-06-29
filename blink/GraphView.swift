79ac852bd883aeca168b3d749aa5ea24f9000f07
Monash Sapkota <monash.sapkota@gmail.com>
2024-06-30 07:37:32 +1000

    added date period to chart

--- blink/GraphView.swift
+++ blink/GraphView.swift
@@ -15,6 +15,23 @@ struct GraphView: View {
         let totalScore = eyeScores.reduce(0) { $0 + $1.score }
         return Double(totalScore) / Double(eyeScores.count)
     }
+    let currentDate = Date()
+    let sevenDaysAgo = Calendar.current.date(byAdding: .day, value: -7, to: Date())!
+
+    // Computed properties for formatted dates
+    private var formattedCurrentDate: String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MMM d, yyyy" // Example format: Jan 1, 2023
+        return dateFormatter.string(from: Date())
+    }
+
+    private var formattedSevenDaysAgo: String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MMM d, yyyy" // Example format: Jan 1, 2023
+        let sevenDaysAgo = Calendar.current.date(byAdding: .day, value: -7, to: Date())!
+        return dateFormatter.string(from: sevenDaysAgo)
+    }
+    
 
     var body: some View {
         VStack {
@@ -27,6 +44,7 @@ struct GraphView: View {
                 .cornerRadius(10) // Rounds the corners of the background
                 .padding() // Adds more padding around the entire element for spacing
                 
+                Divider()
             HStack {
                 Image(systemName: "chevron.left")
                     .onTapGesture {
@@ -36,11 +54,17 @@ struct GraphView: View {
                     .onTapGesture {
                         // Handle right arrow tap, e.g., show next week
                     }
+
+                Text("\(formattedSevenDaysAgo) - \(formattedCurrentDate)")
+                    .font(.caption)
+                    .fontWeight(.bold) // Makes the font bold
+                    .padding(.horizontal, 20) // Adjust padding as needed
+                    .padding(.vertical, 10)
+                    
             }
                 .padding(.leading) // Adjust padding as needed to align with the Chart
                 .padding(.vertical, 10) // Adds vertical padding
                 .frame(maxWidth: .infinity, alignment: .leading) // Align HStack to the left
-            Divider()
             Text("Avg: \(String(format: "%.1f", averageScore))%")
                 .font(.caption)
                 .foregroundColor(.blue)
